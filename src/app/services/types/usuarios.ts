a01f41465de0518d1e55f83b4dd55f30da7fa534
= <=>
2022-11-21 12:32:51 -0400

    cambios 21/11/22

--- src/app/services/types/usuarios.ts
+++ src/app/services/types/usuarios.ts
@@ -1,12 +1,13 @@
 export type Materia = {
-    id_m:number,
-    nombre:string,
-    url:string,
-    grado:string,
-    costo:number,
-    id_semestre:number,
-    estado:number,
-    fecha_desde:Date,
-    fecha_hasta:Date,
-    descripcion:string
+    id_m: number,
+    nombre: string,
+    url: string,
+    grado: string,
+    costo: number,
+    id_semestre: number,
+    estado: number,
+    fecha_desde: Date,
+    fecha_hasta: Date,
+    descripcion: string,
+    duracion: number
 }
